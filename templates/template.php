eb5a1586f377e208a0385575a1005f4944125aee
coder <zenfulcoder@gmail.com>
2020-02-15 20:25:08 -0800

    template updates

--- templates/template.php
+++ templates/template.php
@@ -1,52 +1,135 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <title>{{report_title}}</title>
-    </head>
-    <body>
-        <table style="border:0px;width:100%; text-align:center;">
-            <tr>
-                <th colspan="2" style="width:100%; background-color:#CCCCCC;">
-                    <h1 style="width:100%; background-color:#CCCCCC;">{{report_title}}</h1>
-                    <h3 style="width:100%; background-color:#CCCCCC;">{{report_subtitle}}</h3>
-                    <h4 style="padding-top:2px;">By MJB Code LLC</h4>
-                </th>
-            </tr>
-        </table>
-        <table style="border:0px;width:100%; text-align:left;">
-            <tr>
-                <th colspan="2" style="width:100%; background-color:#CCCCCC; text-align:center;vertical-align:middle;"><h1>New Movies</h1></th>
-            </tr>
-            {{movies}}
-        </table>
-        <table style="border:0px;width:100%; text-align:left;">
-            <tr>
-                <th colspan="2" style="width:100%; background-color:#CCCCCC; text-align:center;vertical-align:middle;"><h1>New TV Shows</h1></th>
-            </tr>
-            {{shows}}
-        </table>
-        <table style="border:0px;width:100%; text-align:center;">
-			<tr>
-                <th colspan="2" style="width:100%; background-color:#CCCCCC; ">
-                    <p style="width:100%; background-color:#CCCCCC; text-align:center;">
-                        Summary<br />
-                       	Last Updated: {{last_updated}}<br />
-						Duration: {{duration}} minutes
-                    </p>
-                </th>
-            </tr>
-            <tr>
-                <th colspan="2" style="width:100%; background-color:#CCCCCC; ">
-                    <p style="width:100%; background-color:#CCCCCC; text-align:center;">
-                        Credits<br />
-                        <a href="https://www.themoviedb.org/" target="_blank">The Movie DB</a><br />
-                        <a href="https://www.thetvdb.com/" target="_blank">The TV DB</a><br />
-                        <a href="http://www.omdbapi.com/" target="_blank">OMBD API</a><br />
-                        <a href="https://github.com/Dachande663/Plex-Export/blob/master/cli.php" target="_blank">Plex Export CLI</a>
-                    </p>
-                </th>
-            </tr>
-        </table>
-    </body>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="x-apple-disable-message-reformatting">
+    <title>{{report_title}}</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+
+    <style type="text/css">
+        body,html{margin:0 auto!important;padding:0!important;height:100%!important;width:100%!important;background:#f1f1f1}*{-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%}div[style*="margin: 16px 0"]{margin:0!important}table,td{mso-table-lspace:0!important;mso-table-rspace:0!important}table{border-spacing:0!important;border-collapse:collapse!important;table-layout:fixed!important;margin:0 auto!important}img{-ms-interpolation-mode:bicubic}a{text-decoration:none}.aBn,.unstyle-auto-detected-links *,[x-apple-data-detectors]{border-bottom:0!important;cursor:default!important;color:inherit!important;text-decoration:none!important;font-size:inherit!important;font-family:inherit!important;font-weight:inherit!important;line-height:inherit!important}.a6S{display:none!important;opacity:.01!important}.im{color:inherit!important}img.g-img+div{display:none!important}@media only screen and (min-device-width:320px) and (max-device-width:374px){u~div .email-container{min-width:320px!important}}@media only screen and (min-device-width:375px) and (max-device-width:413px){u~div .email-container{min-width:375px!important}}@media only screen and (min-device-width:414px){u~div .email-container{min-width:414px!important}}.bg_white{background:#fff}.bg_light{background:#fafafa}.bg_black{background:#000}.bg_dark{background:rgba(0,0,0,.8)}.email-section{padding:2em 2.0em 0em}.btn{padding:5px 15px;display:inline-block}.btn.btn-primary{border-radius:30px;background:#448ef6;color:#fff}.btn.btn-white{border-radius:30px;background:#fff;color:#000}.btn.btn-white-outline{border-radius:30px;background:0 0;border:1px solid #fff;color:#fff}h1,h2,h3,h4,h5,h6{font-family:'Lato',sans-serif;color:#000;margin-top:0;font-weight:400}body{font-family:'Lato',sans-serif;font-weight:400;font-size:15px;line-height:1.8;color:rgba(0,0,0,.4)}a{color:#448ef6}.logo{margin:0;display:inline-block;position:absolute;top:10px;left:0;right:0;margin-bottom:0}.logo a{color:#fff;font-size:24px;font-weight:700;text-transform:uppercase;font-family:'Lato',sans-serif;display:inline-block;border:2px solid #fff;line-height:1.3;padding:10px 15px 4px 15px;margin:0}.logo h1 a span{line-height:1}.navigation{padding:0}.navigation li{list-style:none;display:inline-block;margin-left:5px;font-size:13px;font-weight:500}.navigation li a{color:rgba(0,0,0,.4)}.hero{position:relative;z-index:0}.hero .overlay{position:absolute;top:0;left:0;right:0;bottom:0;content:'';width:100%;background:#000;z-index:-1;opacity:.3}.hero .text{color:rgba(255,255,255,.9)}.hero .text h2{color:#fff;font-size:40px;margin-bottom:0;font-weight:600;line-height:1;text-transform:uppercase}.hero .text h2 span{font-weight:600;color:#448ef6}.heading-section h2{color:#000;font-size:28px;margin-top:0;line-height:1.4;font-weight:700;text-transform:uppercase;letter-spacing:1px}.heading-section .subheading{margin-bottom:20px!important;display:inline-block;font-size:13px;text-transform:uppercase;letter-spacing:2px;color:rgba(0,0,0,.4);position:relative}.heading-section .subheading::after{position:absolute;left:0;right:0;bottom:-10px;content:'';width:100%;height:2px;background:#448ef6;margin:0 auto}.heading-section-white{color:rgba(255,255,255,.8)}.heading-section-white h2{font-family:line-height: 1;padding-bottom:0}.heading-section-white h2{color:#fff}.heading-section-white .subheading{margin-bottom:0;display:inline-block;font-size:13px;text-transform:uppercase;letter-spacing:2px;color:rgba(255,255,255,.4)}.blog-entry{border:1px solid red;padding-bottom:30px !important!important}.text-blog .meta{text-transform:uppercase;font-size:13px;margin-bottom:0}.footer{color:rgba(255,255,255,.5)}.footer .heading{color:#fff;font-size:20px}.footer ul{margin:0;padding:0}.footer ul li{list-style:none;margin-bottom:10px}.footer ul li a{color:rgba(255,255,255,1)}
+    </style>
+</head>
+
+<body width="100%" style="margin: 0; padding: 0 !important; mso-line-height-rule: exactly; background-color: #222222;">
+    <center style="width: 100%; background-color: #f1f1f1;">
+        <div style="max-width: 768px; margin: 0 auto;" class="email-container">
+            <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;">
+                <tr>
+                    <td valign="middle" class="hero bg_white" style="height: 200px;">
+                        <div class="overlay"></div>
+                        <table>
+                            <tr>
+                                <td>
+                                    <div class="text" style="padding: 0 4em; text-align: center;">
+                                        <h2>
+                                            {{report_title}}<br />
+                                            <span style="font-size: 50%;color:#fff;">{{report_subtitle}}</span><br />
+                                            <span style="font-size: 50%;color:#fff;">By MJB Code LLC</span>
+                                        </h2>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr> 
+                <tr>
+                    <td class="bg_white email-section">
+                        <div class="heading-section bg_light" style="text-align: center; padding: 30px 30px 1px 30px; border:2px solid black;">
+                            <h2>New Movies</h2>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="bg_white email-section">
+                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+                            {{movies}}
+                        </table>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="bg_white email-section">
+                        <div class="heading-section bg_light" style="text-align: center; padding: 30px 30px 1px 30px; border:2px solid black;">
+                            <h2>New TV Shows</h2>
+                        </div>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="bg_white email-section">
+                        <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="100%">
+                            {{shows}}
+                        </table>
+                    </td>
+                </tr>
+            </table>
+
+            <table align="center" role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%" style="margin: auto;">
+                <tr>
+                    <td valign="middle" class="bg_black footer email-section">
+                        <center>
+                            <table>
+                                <tr>
+                                    <td valign="top" width="40%" style="padding-top: 10px;">
+                                      <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                                        <tr>
+                                          <td style="text-align: left; padding-right: 10px;">
+                                            <h3 class="heading">Summary</h3>
+                                            <p>
+                                                <ul>
+                                                    <li style="color: #fff;"><strong>Last Updated:</strong> {{last_updated}}</li>
+                                                    <li style="color: #fff;"><strong>Duration:</strong> {{duration}} minutes</li>
+                                                <ul>
+                                            </p>
+                                          </td>
+                                        </tr>
+                                      </table>
+                                    </td>
+                                    <td valign="top" width="40%" style="padding-top: 10px;">
+                                      <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                                        <tr>
+                                          <td style="text-align: left; padding-right: 10px;">
+                                            <h3 class="heading">Credits</h3>
+                                            <p>
+                                                <ul>
+                                                    <li><a href="https://www.themoviedb.org/" target="_blank">The Movie DB</a></li>
+                                                    <li><a href="https://www.thetvdb.com/" target="_blank">The TV DB</a></li>
+                                                    <li><a href="http://www.omdbapi.com/" target="_blank">OMBD API</a></li>
+                                                    <li><a href="https://github.com/Dachande663/Plex-Export/blob/master/cli.php" target="_blank">Plex Export CLI</a></li>
+                                                <ul>
+                                            </p>
+                                          </td>
+                                        </tr>
+                                      </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </center>
+                    </td>
+                </tr>
+                <tr>
+                    <td valign="middle" class="bg_black footer email-section">
+                        <table>
+                            <tr>
+                                <td valign="top">
+                                    <center>
+                                        <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
+                                            <tr>
+                                                <td style="text-align: left; padding-right: 10px;">
+                                                    <p>&copy; 2020 MJB Code LLC. All Rights Reserved</p>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </center>
+                                </td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </center>
+</body>
 </html>
